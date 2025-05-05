d09d75aaf32331e4f7c48626862ecc2cc76f6839
mdp <mh375200@dal.ca>
2025-05-05 00:39:02 -0300

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public static String[] validCommands = {"type", "exit", "echo"};
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
 
@@ -22,8 +24,19 @@ public class Main {
         String[] words = input.split("\\s+");
         if(words[0].equalsIgnoreCase("echo")) {
             printEcho(words);
-        } else {
+        } else if (words[0].equalsIgnoreCase("type")) {
+            checkType(words[1].toLowerCase());
+        }
+        else {
             System.out.println(input + " command not found");
+        }
+    }
+
+    private static void checkType(String word) {
+        if( Arrays.asList(validCommands).contains(word)) {
+            System.out.println(word + " is a shell builtin");
+        } else {
+            System.out.println(word + ": not found");
         }
     }
 
