7323e8c0856a69aec33dd0703708e525b7e05d17
Shadow2121 <mihir20011patel@gmail.com>
2025-12-09 13:38:47 -0400

    append 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -26,23 +29,47 @@ public class Main {
     }
 
     private static void processInput(String input) {
-        String[] tokens = input.split(">");
+        String[] tokens = input.split(" >");
+        boolean append = false;
         if(tokens.length == 2 && tokens[0].endsWith("2")) {
-            String filePath = tokens[1].trim();
+            if(tokens[1].startsWith(">")) {
+                append = true;
+                tokens[1] = tokens[1].substring(1);
+            }
+            if(tokens[0].endsWith("2")) {
+                tokens[0] = tokens[0].substring(0, tokens[0].length()-1).trim();
+            }
+            String fileName = tokens[1].trim();
+            Path filePath = Paths.get(fileName);
             try {
-                File file = new File(filePath);
-                file.createNewFile();
-            } catch (IOException _) {}
-            processCommand(tokens[0].substring(0, tokens[0].length()-1).trim(), filePath, 2);
+                if(!Files.exists(filePath)) {
+                    Files.createFile(filePath);
+                }
+
+                processCommand(tokens[0].trim(), fileName, 2, append);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } else if(tokens.length == 2) {
-            String filePath = tokens[1].trim();
+            if(tokens[1].startsWith(">")) {
+                append = true;
+                tokens[1] = tokens[1].substring(1);
+            }
+            if(tokens[0].endsWith("1")) {
+                tokens[0] = tokens[0].substring(0, tokens[0].length()-1).trim();
+            }
+            String fileName = tokens[1].trim();
+            Path filePath = Paths.get(fileName);
             try {
-                File file = new File(filePath);
-                file.createNewFile();
-            } catch (IOException _) {}
-            processCommand(tokens[0].substring(0, tokens[0].length()-1).trim(), filePath, 1);
+                if(!Files.exists(filePath)) {
+                    Files.createFile(filePath);
+                }
+                processCommand(tokens[0].trim(), fileName, 1, append);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } else {
-            processCommand(input, null, 0);
+            processCommand(input, null, 0, append);
         }
     }
 
@@ -81,26 +108,26 @@ public class Main {
         return args;
     }
 
-    private static void processCommand(String input, String filePath, int redirectNumber) {
+    private static void processCommand(String input, String filePath, int redirectNumber, boolean append) {
         ArrayList<String> args = extractArgs(input);
 
         if(args.getFirst().equalsIgnoreCase("echo")) {
-            printEcho(args, filePath, redirectNumber);
+            printEcho(args, filePath, redirectNumber, append);
         } else if (args.getFirst().equalsIgnoreCase("type")) {
-            checkType(args.get(1).toLowerCase(), filePath, redirectNumber);
+            checkType(args.get(1).toLowerCase(), filePath, redirectNumber, append);
         } else if (args.getFirst().equalsIgnoreCase("pwd")) {
-            printWorkingDir(filePath, redirectNumber);
+            printWorkingDir(filePath, redirectNumber, append);
         } else if (args.getFirst().equalsIgnoreCase("cd")) {
-            changeDir(args.get(1), filePath, redirectNumber);
+            changeDir(args.get(1), filePath, redirectNumber, append);
         } else if (args.getFirst().equalsIgnoreCase("cat")) {
-            openFileAndPrint(args, filePath, redirectNumber);
+            openFileAndPrint(args, filePath, redirectNumber, append);
         }
         else {
-            executeAnyCommand(args, filePath, redirectNumber);
+            executeAnyCommand(args, filePath, redirectNumber, append);
         }
     }
 
-    private static void executeAnyCommand(ArrayList<String> args, String filePath, int redirectNumber) {
+    private static void executeAnyCommand(ArrayList<String> args, String fileName, int redirectNumber, boolean append) {
         try {
             ProcessBuilder processBuilder = new ProcessBuilder(args); // For Windows
 
@@ -112,8 +139,13 @@ public class Main {
             InputStream inputStream = process.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line;
-            if(filePath != null && redirectNumber == 1) {
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            if(fileName != null && redirectNumber == 1) {
+                Path filePath = Paths.get(fileName);
+                try (BufferedWriter writer = Files.newBufferedWriter(
+                        filePath,
+                        StandardOpenOption.CREATE, // Creates the file if it doesn't exist
+                        (append) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING // Ensures we append, not overwrite
+                )) {
                     while ((line = reader.readLine()) != null) {
                         writer.write(line);
                         writer.newLine();
@@ -123,8 +155,13 @@ public class Main {
                 }
             } else {
                 while ((line = reader.readLine()) != null) {
-                    if(line.endsWith("No such file or directory") && filePath != null && redirectNumber == 2) {
-                        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                    if(line.endsWith("No such file or directory") && fileName != null && redirectNumber == 2) {
+                        Path filePath = Paths.get(fileName);
+                        try (BufferedWriter writer = Files.newBufferedWriter(
+                                filePath,
+                                StandardOpenOption.CREATE, // Creates the file if it doesn't exist
+                                (append) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING  // Ensures we append, not overwrite
+                        )) {
                             writer.write(line);
                             writer.newLine();
                         } catch (IOException e) {
@@ -136,8 +173,13 @@ public class Main {
                 }
             }
         } catch (Exception _) {
-            if(filePath != null && redirectNumber == 2) {
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            if(fileName != null && redirectNumber == 2) {
+                Path filePath = Paths.get(fileName);
+                try (BufferedWriter writer = Files.newBufferedWriter(
+                        filePath,
+                        StandardOpenOption.CREATE, // Creates the file if it doesn't exist
+                        (append) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING // Ensures we append, not overwrite
+                )) {
                     writer.write(args.getFirst() + ": not found");
                     writer.newLine();
                 } catch (IOException e) {
@@ -149,7 +191,7 @@ public class Main {
         }
     }
 
-    private static void openFileAndPrint(ArrayList<String> words, String filePath, int redirectNumber) {
+    private static void openFileAndPrint(ArrayList<String> words, String fileName, int redirectNumber, boolean append) {
         for(int i = 1; i < words.size(); i++) {
             File file = getAbsoluteFile(words.get(i));
             if(file != null && file.exists() && file.isFile()) {
@@ -157,8 +199,13 @@ public class Main {
                     // Read all content of the file into a String
                     String fileContent = Files.readString(file.getAbsoluteFile().toPath());
 
-                    if(filePath != null && redirectNumber == 1) {
-                        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                    if(fileName != null && redirectNumber == 1) {
+                        Path filePath = Paths.get(fileName);
+                        try (BufferedWriter writer = Files.newBufferedWriter(
+                                filePath,
+                                StandardOpenOption.CREATE, // Creates the file if it doesn't exist
+                                (append) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING  // Ensures we append, not overwrite
+                        )) {
                             writer.write(fileContent);
                         } catch (IOException e) {
                             System.err.println("An error occurred while writing to the file: " + e.getMessage());
@@ -172,8 +219,13 @@ public class Main {
                     System.err.println("Error reading the file: " + e.getMessage());
                 }
             } else {
-                if(filePath != null && redirectNumber == 2) {
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                if(fileName != null && redirectNumber == 2) {
+                    Path filePath = Paths.get(fileName);
+                    try (BufferedWriter writer = Files.newBufferedWriter(
+                            filePath,
+                            StandardOpenOption.CREATE, // Creates the file if it doesn't exist
+                            (append) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING // Ensures we append, not overwrite
+                    )) {
                         writer.write("cat: " + words.get(i) + ": No such file or directory");
                         writer.newLine();
                     } catch (IOException e) {
@@ -186,10 +238,15 @@ public class Main {
         }
     }
 
-    private static void printWorkingDir(String filePath, int redirectNumber) {
+    private static void printWorkingDir(String fileName, int redirectNumber, boolean append) {
         String currentDirectory = System.getProperty("user.dir");
-        if(filePath != null && redirectNumber == 1) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+        if(fileName != null && redirectNumber == 1) {
+            Path filePath = Paths.get(fileName);
+            try (BufferedWriter writer = Files.newBufferedWriter(
+                    filePath,
+                    StandardOpenOption.CREATE, // Creates the file if it doesn't exist
+                    (append) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING  // Ensures we append, not overwrite
+            )) {
                 writer.write(currentDirectory);
                 writer.newLine();
             } catch (IOException e) {
@@ -200,7 +257,7 @@ public class Main {
         }
     }
 
-    private static void checkType(String word, String filePath, int redirectNumber) {
+    private static void checkType(String word, String fileName, int redirectNumber, boolean append) {
         String output = null;
         String error = null;
         if(word.equalsIgnoreCase("cat")) {
@@ -215,8 +272,13 @@ public class Main {
                 error = word + ": not found";
             }
         }
-        if(filePath != null && output != null && redirectNumber == 1) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+        if(fileName != null && output != null && redirectNumber == 1) {
+            Path filePath = Paths.get(fileName);
+            try (BufferedWriter writer = Files.newBufferedWriter(
+                    filePath,
+                    StandardOpenOption.CREATE, // Creates the file if it doesn't exist
+                    (append) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING  // Ensures we append, not overwrite
+            )) {
                 writer.write(output);
                 writer.newLine();
             } catch (IOException e) {
@@ -226,8 +288,13 @@ public class Main {
             System.out.println(output);
         }
         if(error != null ) {
-            if(filePath != null && redirectNumber == 2) {
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            if(fileName != null && redirectNumber == 2) {
+                Path filePath = Paths.get(fileName);
+                try (BufferedWriter writer = Files.newBufferedWriter(
+                        filePath,
+                        StandardOpenOption.CREATE, // Creates the file if it doesn't exist
+                        (append) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING  // Ensures we append, not overwrite
+                )) {
                     writer.write(error);
                     writer.newLine();
                 } catch (IOException e) {
@@ -261,9 +328,14 @@ public class Main {
         return null;
     }
 
-    private static void printEcho(ArrayList<String> args, String filePath, int redirectNumber) {
-        if(filePath != null && redirectNumber == 1) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+    private static void printEcho(ArrayList<String> args, String fileName, int redirectNumber, boolean append) {
+        if(fileName != null && redirectNumber == 1) {
+            Path filePath = Paths.get(fileName);
+            try (BufferedWriter writer = Files.newBufferedWriter(
+                    filePath,
+                    StandardOpenOption.CREATE, // Creates the file if it doesn't exist
+                    (append) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING  // Ensures we append, not overwrite
+            )) {
                 writer.write(String.join(" ", args).substring(4).trim());
                 writer.newLine();
             } catch (IOException e) {
@@ -274,7 +346,7 @@ public class Main {
         }
     }
 
-    private static void changeDir(String path, String filePath, int redirectNumber) {
+    private static void changeDir(String path, String fileName, int redirectNumber, boolean append) {
         if(path.equals("~")) {
             System.setProperty("user.dir", System.getenv("HOME"));
         } else {
@@ -282,8 +354,13 @@ public class Main {
             if(folder != null && folder.isDirectory()) {
                 System.setProperty("user.dir", folder.getAbsolutePath());
             } else {
-                if(filePath != null && redirectNumber == 2) {
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                if(fileName != null && redirectNumber == 2) {
+                    Path filePath = Paths.get(fileName);
+                    try (BufferedWriter writer = Files.newBufferedWriter(
+                            filePath,
+                            StandardOpenOption.CREATE, // Creates the file if it doesn't exist
+                            (append) ? StandardOpenOption.APPEND : StandardOpenOption.TRUNCATE_EXISTING  // Ensures we append, not overwrite
+                    )) {
                         writer.write("cd: " + path + ": No such file or directory");
                         writer.newLine();
                     } catch (IOException e) {
